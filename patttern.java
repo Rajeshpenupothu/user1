bf9dbcef6079bc10ebd6234edbd5ce5f15d211ac
rajeshpenupothu <rajeshpenupothu@gmail.com>
2025-02-20 08:50:27 +0530

    hollow square

--- patttern.java
+++ patttern.java
@@ -1,1 +1,16 @@
-<h1>Rajesh loves vennela</h1>
+import java.util.Scanner;
+public class patttern{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("$ ");
+            }
+            System.out.println( );
+        }
+    }
+}
